af7fae52f39be0defd1b3ee2517ea5fc63e2b625
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-26 23:39:45 -0700

    update sample test cases

--- test_parse_opening_hours.py
+++ test_parse_opening_hours.py
@@ -6,28 +6,48 @@ class TestHoursParsing(unittest.TestCase):
 	mon_9_to_5 = {
 		"day": "monday",
 		"opens": "9:00",
-		"closes": "5:00"
+		"closes": "17:00"
 	}
 
 	tue_9_to_5 = {
 		"day": "tuesday",
 		"opens": "9:00",
-		"closes": "5:00"
+		"closes": "17:00"
 	}
 
 	wed_9_to_5 = {
 		"day": "wednesday",
 		"opens": "9:00",
-		"closes": "5:00"
+		"closes": "17:00"
 	}
 
-	allweek_9_to_5 = []
-	for day in ["monday", "tuesday", "wednesday", "thursday", "friday", " saturday", "sunday"]:
-		allweek_9_to_5.append({
-			"day": day,
-			"opens": "9:00",
-			"closes": "5:00"
-		})
+	thurs_9_to_5 = {
+		"day": "wednesday",
+		"opens": "9:00",
+		"closes": "17:00"
+	}
+
+	fri_9_to_5 = {
+		"day": "wednesday",
+		"opens": "9:00",
+		"closes": "17:00"
+	}
+
+	sat_9_to_5 = {
+		"day": "wednesday",
+		"opens": "9:00",
+		"closes": "17:00"
+	}
+
+	sun_9_to_5 = {
+		"day": "wednesday",
+		"opens": "9:00",
+		"closes": "17:00"
+	}
+
+	workweek_9_to_5 = [mon_9_to_5, tue_9_to_5, wed_9_to_5,thurs_9_to_5, fri_9_to_5]
+	allweek_9_to_5 = workweek_9_to_5.extend([sat_9_to_5, sun_9_to_5])
+
 
 	def test_single_day_miltime(self):
 		# TODO: implement assumption of pm if end time <= start time
@@ -45,7 +65,7 @@ class TestHoursParsing(unittest.TestCase):
 			"Mon 9-5",
 			"M 9-5"
 		]
-		expected_result = [ mon_9_to_5 ]
+		expected_result = [ self.mon_9_to_5 ]
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 
@@ -58,7 +78,7 @@ class TestHoursParsing(unittest.TestCase):
 			"Mon 9:00am - 5:00pm",
 			"M 9:00am - 5:00pm"
 		]
-		expected_result = [ mon_9_to_5 ]
+		expected_result = [ self.mon_9_to_5 ]
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 
@@ -74,7 +94,7 @@ class TestHoursParsing(unittest.TestCase):
 			"Monday 9am to 5pm",
 			"Monday 9am through 5pm"
 		]
-		expected_result = [ mon_9_to_5 ]
+		expected_result = [ self.mon_9_to_5 ]
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 	
@@ -92,7 +112,7 @@ class TestHoursParsing(unittest.TestCase):
 			"Monday to Friday 9:00 - 5:00",
 
 		]
-		expected_result = [ mon_9_to_5, tue_9_to_5, wed_9_to_5 ]
+		expected_result = [ self.mon_9_to_5, self.tue_9_to_5, self.wed_9_to_5 ]
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 
@@ -111,7 +131,7 @@ class TestHoursParsing(unittest.TestCase):
 			"9:00am - 5:00pm 7 days",
 			"9:00am - 5:00pm Every Day"
 		]
-		expected_result = allweek_9_to_5
+		expected_result = self.allweek_9_to_5
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 
@@ -127,14 +147,14 @@ class TestHoursParsing(unittest.TestCase):
 			"9:00am - 5:00pm 5 days",
 			"9:00am - 5:00pm Business Days"
 		]
-		expected_result = allweek_9_to_5
+		expected_result = self.workweek_9_to_5
 		for input_str in input_strings:
 			self.assertEqual(JsonOpeningHours.parse(input_str), expected_result)
 
 	def test_create_entry(self):
 		self.assertEqual(
 			create_entry("monday", "9:00", "5:00"),
-			mon_9_to_5
+			self.mon_9_to_5
 		)
 
 	def test_expand_day_range(self):
