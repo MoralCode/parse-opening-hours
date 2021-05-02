c71a23677c2b3bc87c481f46ab682c56be6d83e9
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-01 22:25:16 -0700

    move more helpers over to helpers file

--- helpers.py
+++ helpers.py
@@ -1,4 +1,52 @@
-
+from parse_opening_hours import Weekday
 
 def detect_if_pm(string):
 	return "p" in string.lower()
+
+
+def str_to_day(day_string):
+	if day_string is None:
+		return None
+	day = day_string.lower()
+	if day in ["m", "mon", "monday"]:
+		return Weekday.MONDAY
+	elif day in ["t", "tue", "tues", "tuesday"]:
+		return Weekday.TUESDAY
+	elif day in ["w", "wed", "wednesday"]:
+		return Weekday.WEDNESDAY
+	elif day in ["th", "thu", "thurs", "thusday"]:
+		return Weekday.THURSDAY
+	elif day in ["f", "fri", "friday"]:
+		return Weekday.FRIDAY
+	elif day in ["sa", "sat", "saturday"]:
+		return Weekday.SATURDAY
+	elif day in ["su", "sun", "sunday"]:
+		return Weekday.SUNDAY
+
+def day_to_str(day):
+	if day == Weekday.MONDAY:
+		return "monday"
+	elif day == Weekday.TUESDAY:
+		return "tuesday"
+	elif day == Weekday.WEDNESDAY:
+		return "wednesday"
+	elif day == Weekday.THURSDAY:
+		return "thursday"
+	elif day == Weekday.FRIDAY:
+		return "friday"
+	elif day == Weekday.SATURDAY:
+		return "saturday"
+	elif day == Weekday.SUNDAY:
+		return "sunday"
+
+
+def expand_day_range(start_day, end_day):
+	if end_day is None:
+		return [start_day]
+	days = []
+	start_index = start_day.value
+	end_index = end_day.value
+
+	for index in range(start_index, end_index+1, 1):
+		days.append(Weekday(index))
+	return days
--- parse_opening_hours.py
+++ parse_opening_hours.py
@@ -11,7 +11,7 @@
 
 from pyparsing import Word, alphas, nums, oneOf, Optional, Or, OneOrMore, Char
 from patterns import *
-from helpers import detect_if_pm
+from helpers import detect_if_pm, str_to_day, day_to_str, expand_day_range
 import enum
 
 class Weekday(enum.Enum):
@@ -50,52 +50,6 @@ def create_entry(day, opening, closing, notes=None):
 		entry["notes"] = notes
 	return entry
 
-def str_to_day(day_string):
-	if day_string is None:
-		return None
-	day = day_string.lower()
-	if day in ["m", "mon", "monday"]:
-		return Weekday.MONDAY
-	elif day in ["t", "tue", "tues", "tuesday"]:
-		return Weekday.TUESDAY
-	elif day in ["w", "wed", "wednesday"]:
-		return Weekday.WEDNESDAY
-	elif day in ["th", "thu", "thurs", "thusday"]:
-		return Weekday.THURSDAY
-	elif day in ["f", "fri", "friday"]:
-		return Weekday.FRIDAY
-	elif day in ["sa", "sat", "saturday"]:
-		return Weekday.SATURDAY
-	elif day in ["su", "sun", "sunday"]:
-		return Weekday.SUNDAY
-
-def day_to_str(day):
-	if day == Weekday.MONDAY:
-		return "monday"
-	elif day == Weekday.TUESDAY:
-		return "tuesday"
-	elif day == Weekday.WEDNESDAY:
-		return "wednesday"
-	elif day == Weekday.THURSDAY:
-		return "thursday"
-	elif day == Weekday.FRIDAY:
-		return "friday"
-	elif day == Weekday.SATURDAY:
-		return "saturday"
-	elif day == Weekday.SUNDAY:
-		return "sunday"
-
-
-def expand_day_range(start_day, end_day):
-	if end_day is None:
-		return [start_day]
-	days = []
-	start_index = start_day.value
-	end_index = end_day.value
-
-	for index in range(start_index, end_index+1, 1):
-		days.append(Weekday(index))
-	return days
 
 def parse_times(result):
 	# assumes that all three (hours, minutes, am_pm) are the same length
