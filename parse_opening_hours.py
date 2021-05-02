8984ddfaee15bb965225fa54de0d91c41407e37c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-01 22:21:02 -0700

    move common patterns to a separate file

--- parse_opening_hours.py
+++ parse_opening_hours.py
@@ -10,6 +10,7 @@
 #   ]
 
 from pyparsing import Word, alphas, nums, oneOf, Optional, Or, OneOrMore, Char
+from patterns import *
 from helpers import detect_if_pm
 import enum
 
@@ -26,27 +27,6 @@ class JsonOpeningHours():
 
 	@classmethod
 	def parse(self, hours_string):
-
-		range_separator = Or([Word(" –—‐-"), oneOf("to thru through until")])
-		#TODO: support multiple sections like M 8am-2pm, W 9am-2pm
-		section_separator = Optional(",")
-		time_separator = Optional(":")
-		day = Word(alphas)
-		time_number = Word(nums, max=2)
-		am_or_pm = Optional(Word("AaPpMm.").setResultsName('am_pm', listAllMatches=True))
-
-		hour = time_number("hour*")
-		minute = time_number("minute*")
-		time_minutes = Optional(time_separator + minute)
-		# miltime = milhour.setResultsName('hour_24') + time_minutes
-		
-		time = hour + time_minutes + am_or_pm
-
-		daterange = day.setResultsName('startday', listAllMatches=True) + Optional(range_separator + day.setResultsName('endday', listAllMatches=True))
-
-		timerange = time.setResultsName('starttime', listAllMatches=True) + Optional(range_separator + time.setResultsName('endtime', listAllMatches=True))
-
-		notes = section_separator + Optional(OneOrMore(Word(alphas))).setResultsName('notes', listAllMatches=True)
 
 		opening_hours_format = Or([
 			OneOrMore(daterange + timerange + notes),
