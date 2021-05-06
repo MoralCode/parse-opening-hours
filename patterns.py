0f28e43db494a15bc35100cca6547020176bce16
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-06 11:47:13 -0700

    set up separate patterns for date range and date list

--- patterns.py
+++ patterns.py
@@ -24,7 +24,11 @@ am_or_pm = Optional(Word("AaPpMm.").setResultsName('am_pm', listAllMatches=True)
 
 time = time_number("hour*") + time_separator + Optional(time_number("minute*")) + am_or_pm
 
-daterange = day.setResultsName('startday', listAllMatches=True) + Optional(range_separator + day.setResultsName('endday', listAllMatches=True))
+dateList = OneOrMore(day.setResultsName('day', listAllMatches=True) + Optional(list_separator))
+
+daterange = day.setResultsName('startday', listAllMatches=True) + range_separator + day.setResultsName('endday', listAllMatches=True)
+
+dates = Or([daterange, dateList])
 
 timerange = time.setResultsName('starttime', listAllMatches=True) + Optional(range_separator + time.setResultsName('endtime', listAllMatches=True))
 
