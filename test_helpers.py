454c0c8c354bddd1aeb13f68ab8369fd25a1eea9
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-06 14:44:43 -0700

    add test for str_to_day helper

--- test_helpers.py
+++ test_helpers.py
@@ -8,6 +8,20 @@ class TestHelpers(unittest.TestCase):
 			expand_day_range(Weekday.MONDAY, Weekday.FRIDAY),
 			[Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY]
 		)
+	
+	def test_str_to_day(self):
+		input_strings = [
+			"Monday",
+			"Mondays",
+			"Monday's",
+			"Mondays'",
+			"Mon",
+			"mon",
+			"M"
+		]
+		expected_result = Weekday.MONDAY
+		for instr in input_strings:
+			self.assertEqual(str_to_day(instr), expected_result)
 
 if __name__ == '__main__':
 	unittest.main()
