2871958356c9b75b0c1b66696ef785098eea9a62
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-20 23:43:06 -0700

    update test exclusion to not be too excessive

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=["pyparsing",],
 )
