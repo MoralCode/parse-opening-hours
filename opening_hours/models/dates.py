76a346a2703f9c0f53d6c090ad42710e983f1ed7
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-07-07 17:17:58 -0700

    add work in progress dates class with some tests

---
+++ opening_hours/models/dates.py
@@ -0,0 +1,92 @@
+import logging, os
+from dateutil.parser import *
+from datetime import datetime, date as dt_date
+
+logger = logging.getLogger(__name__)
+
+class Dates():
+	"""
+	This class represents a set of datetime.date objects and provides
+	some helpful methods for using them
+	"""
+	dates = set({})
+	@classmethod
+	def from_datetime(cls, datetime):
+		"""
+		create a Dates object from a datetime
+		"""
+		result = cls()
+		result.add(datetime)
+		return result
+			
+	@classmethod
+	def from_parse_results(cls, result, assume_century=None):
+		logger.debug(vars(result))
+		if result is None:
+			raise TypeError("Cannot create Dates Object from value None")
+
+		def _dmy_dict_to_date(dmy_dict):
+			return 
+
+		dates = None
+
+		datesclass = cls()
+
+		# single day
+		# list of days
+
+		if "date" in result:
+			datevalues = result.asDict().get("date")
+			if isinstance(datevalues, list):
+				datevalues = [_dmy_dict_to_date(d) for d in datevalues]
+				# logger.debug(datevalues)
+				for date in datevalues:
+					year = int(dmy_dict.get("year"))
+					if year < 1000 and assume_century:
+						year = (assume_century*100) + year
+
+					datesclass.add(
+						dt_date(
+							year=year,
+							month=int(dmy_dict.get("month")),
+							day=int(dmy_dict.get("day"))
+						)
+					)
+
+			elif isinstance(datevalues, str):
+				datesclass.add(datevalues)
+		
+		return datesclass
+
+	
+		
+	def __init__(self):
+		self.dates = set({})
+		
+	def __str__(self):
+		daystrings = [str(Day(d)) for d in self.dates]
+		return "Dates<" + ''.join(daystrings) + ">"
+
+
+	def add(self, date):
+		if isinstance(date, dt_date):
+			self.dates.add(date)
+		elif isinstance(date, str):
+			self.dates.add(parse(date_str).date())
+		else:
+			# don't attempt to add unrelated types
+			raise NotImplementedError()
+
+	def __iter__(self):
+		return iter(self.sort())
+
+	def sort(self):
+		return sorted(self.dates)
+	
+	def __eq__(self, other):
+		if not isinstance(other, Dates):
+			# don't attempt to compare against unrelated types
+			raise NotImplementedError()
+		return self.dates == other.dates
+
+
---
+++ tests/test_dates.py
@@ -0,0 +1,138 @@
+
+import unittest
+from opening_hours.models.dates import *
+from opening_hours.patterns import specific_dates
+import datetime
+
+
+class TestDates(unittest.TestCase):
+
+	def test_single_day_from_parse_results(self):
+		test_str = "05/06/2021"
+		result = [datetime.date(2021, 5, 6)]
+		parsed = specific_dates.parseString(test_str)
+		self.assertEqual(list(Dates.from_parse_results(parsed).dates), result)
+
+	def test_multi_day_from_parse_results(self):
+		test_str = "05/06/20, 05/07/2020"
+		result = [datetime.date(20, 5, 6), datetime.date(2020, 5, 7)]
+		parsed = specific_dates.parseString(test_str)
+		self.assertEqual(list(Dates.from_parse_results(parsed).dates), result)
+
+	def test_cntury_assumption_from_parse_results(self):
+		test_str = "05/06/2021"
+		result = [datetime.date(2021, 5, 6)]
+		parsed = specific_dates.parseString(test_str)
+		self.assertEqual(list(Dates.from_parse_results(parsed).dates), result)
+
+
+# def test_single_day_from_parse_results(self):
+# 		test_str = [
+# 			"05/06/2021",
+# 			"05/06/21",
+# 		]
+# 		result = [datetime.date(2021, 5, 6)]
+# 		for test in test_str:
+# 			parsed = specific_dates.parseString(test)
+# 			self.assertEqual(list(Dates.from_parse_results(parsed).dates), result)
+
+	# def test_allweek_shortcuts(self):
+	# 	# TODO: implement assumption of pm if end time <= start time
+	# 	input_strings = [
+	# 		"All Week",
+	# 		"7 days a week",
+	# 		"7 days",
+	# 		"Every Day",
+	# 		"",# no date present, just a time -> assume all week
+	# 		"daily",
+	# 		"24/7",
+	# 		# "24 hours a day",
+	# 		"24 hours a day, 7 days a week"
+	# 	]
+	# 	expected_result = self.fullweek
+	# 	self.run_tests(input_strings, expected_result)
+
+	# def test_create_from_none(self):
+	# 	with self.assertRaises(TypeError):
+	# 		Dates.from_shortcut_string(None)
+	# 	with self.assertRaises(TypeError):
+	# 		Dates(None, None)
+	# 	with self.assertRaises(TypeError):
+	# 		Dates.from_parse_results(None)
+
+	# def test_create_from_empty(self):
+	# 	with self.assertRaises(TypeError):
+	# 		Dates("", "")
+
+	# def test_create_from_unknown(self):
+	# 	with self.assertRaises(ValueError):
+	# 		Dates.from_shortcut_string("cheeseburger")
+	
+	# def test_from_parse_regular(self):
+	# 	test_dict = {
+	# 		"hour": 5,
+	# 		"minute": 0,
+	# 		"am_pm": "PM"
+	# 	}
+	# 	test_days_dict = Time.from_parse_results(test_dict)
+	# 	self.assertEqual(
+	# 		test_time_dict.hours,
+	# 		5
+	# 	)
+	# 	self.assertEqual(test_time_dict.minutes, 0)
+
+	# 	self.assertTrue(test_time_dict.is_pm())
+	
+	# def test_from_parse_shortcut(self):
+	# 	test_dict = {
+	# 		"time_shortcuts": "noon"
+	# 	}
+	# 	test_days_dict = Dates.from_parse_results(test_dict)
+	# 	self.assertEqual(
+	# 		test_days_dict.hours,
+	# 		12
+	# 	)
+	# 	self.assertEqual(test_days_dict.minutes, 0)
+
+	# def test_from_parse_unknown(self):
+	# 	test_dict = {
+	# 		"unknown": "dont care"
+	# 	}
+	# 	with self.assertRaises(ValueError):
+	# 		Dates.from_parse_results(test_dict)
+
+	# def test_workweek(self):
+	# 	input_strings = [
+	# 		"Weekdays",
+	# 		"5 days a week",
+	# 		"5 days",
+	# 		"Business Dates",
+	# 		"workdays"
+	# 	]
+	# 	expected_result = self.workweek
+	# 	self.run_tests(input_strings,expected_result)
+
+	# def test_weekend(self):
+	# 	# TODO: implement assumption of pm if end time <= start time
+	# 	input_strings = [
+	# 		"Weekend",
+	# 		"Weekends",
+	# 	]
+	# 	expected_result = self.weekend
+	# 	self.run_tests(input_strings,expected_result)
+
+	# def test_equals(self):
+	# 	workweek = Dates(DatesEnum.MONDAY, DatesEnum.FRIDAY)
+	# 	with self.assertRaises(NotImplementedError):
+	# 		workweek == "something else"
+		
+	# 	self.assertEqual(workweek, workweek)
+
+
+	def run_tests(self, input_strings, expected_result, **kwargs):
+		for input_str in input_strings:
+			print("Testing String: '", input_str)
+			self.assertEqual(list(Dates.from_shortcut_string(input_str, **kwargs)), expected_result)
+
+if __name__ == '__main__':
+	unittest.main()
