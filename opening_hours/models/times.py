55d747e2acf11ea0e04bce8297c4dc5a341e0893
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-07-11 12:51:49 -0700

    remove vars call since it is already a dict

--- opening_hours/models/times.py
+++ opening_hours/models/times.py
@@ -61,7 +61,7 @@ class Times():
 			return cls.from_shortcut_string(result.get("time_shortcuts")[0])
 		else:
 			logger.info("unspecified time pattern detected")
-			logger.debug(vars(result))
+			logger.debug(result)
 			# nothing specified, assumeit means every day
 			return cls(Time(0, 0, TimeType.AM), Time(11, 59, TimeType.PM))
 
